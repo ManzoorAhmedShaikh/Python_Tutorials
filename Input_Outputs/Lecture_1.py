98874d3fd7c7de859ee7ec6cf90ed2dbeb9e3af7
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2023-12-26 10:25:06 +0500

    Lecture 1 updated

---
+++ Input_Outputs/Lecture_1.py
@@ -0,0 +1,18 @@
+
+print("Something")
+print('Something')
+
+input()
+input("Enter some kind of word: ")
+a = input('Enter some kind of word: ')
+
+print("This is stored in variable a: ",a)
+
+"""
+1) Write a program that ask the user for a SECRET CODE
+and then print the secret code which should be in this
+format [FIRSTNAME]_[AGE]_[SECRETCODE]
+"""
+
+Secret_Code = input("Enter some code: ")
+print("[Manzoor]_[6546]_[",Secret_Code,']')
