070aa6f27eaeeadbf483f4ec8ca2cddb503fa5e3
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-02 09:57:41 +0500

    Exercise Basic # 1 Updated

---
+++ Escape_Sequences/Lecture_2.py
@@ -0,0 +1,23 @@
+print("Manzoor")
+print("Ahmed")
+
+print()
+print("Manzoor\nAhmed")
+
+print("Manzoor\\Ahmed")
+
+print("Manzoor\'Ahmed")
+
+print("Manzoor\"Ahmed")
+
+print("Manzoor\t\tAhmed")
+
+print("* * * * *")
+print("*       *")
+print("*       *")
+print("*       *")
+print("* * * * *")
+
+print()
+
+print("* * * * *\n*\t\t*\n*\t\t*\n*\t\t*\n* * * * *")
---
+++ Exercise_Basic_1/Exercise_B1.py
@@ -0,0 +1,30 @@
+"""
+Q1) Write a program in which you have to ask
+S1, S2, S3 and show the output in this way: [(S1|S2)|S3]
+"""
+
+# S1 = input("Enter the value of S1: ")
+# S2 = input("Enter the value of S2: ")
+# S3 = input("Enter the value of S3: ")
+#
+# print("[(",S1,"|",S2,")|",S3,"]")
+
+"""
+* * * *
+*      *
+*      *
+* * * *
+*      *
+*      *
+* * * *
+"""
+
+print("* * * *")
+print("*      *")
+print("*      *")
+print("* * * *")
+print("*      *")
+print("*      *")
+print("* * * *")
+
+print("* * * *\n*\t\t*\n*\t\t*\n* * * *\n*\t\t*\n*\t\t*\n* * * *")
