5ffe2101d2299571c965b6d2f4aa7f94f2273290
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-02 11:12:50 +0500

    Variables and Naming Convention Updated

---
+++ Variables_And_Naming_Convention/Lecture_3.py
@@ -0,0 +1,29 @@
+
+#example
+input()
+
+_a = print("SOMETHING")
+a = input("Enter some value")
+
+#Variable MUST start with either Alphabets or "_"
+
+a = 2
+_a = 2
+_A = 3
+_A2 = 4
+
+#These are not allowed ,.?][=-+
+
+_A%4 = 's'
+AT_2_#3 = ''
+
+#Maximum length of variable can be upto 255 letters
+AKJSDASKJDHASKJDASHDKASJDHAKDJ = 2
+
+#Naming Convention
+My_Name = "Manzoor Ahmed" #SnakeCase
+MyName = "Manzoor Ahmed" #CamelCase
+PI = 3.142 #Constant
+MY_NAME = "Manzoor Ahmed" #Constant
+
+Init_Value = 2
