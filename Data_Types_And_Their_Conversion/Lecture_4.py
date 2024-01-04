d47c1d0f94a5155147d4d13caedb1317dddefbcd
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-04 08:47:20 +0500

    Data Types and Their Conversions Added

---
+++ Data_Types_And_Their_Conversion/Lecture_4.py
@@ -0,0 +1,54 @@
+# Data Types
+
+int()
+str()
+bool()
+float()
+
+#int() Integer
+# 1
+# 2
+# 40
+# 500
+
+#str() String
+# '1'
+# "2"
+# '40'
+# "500"
+
+#Example
+# a = 2
+# b = "3"
+# print(a + b)
+
+#bool() Boolean
+True
+False
+
+#Wrong Statements
+# true TRUE trUE
+
+#float() Floating Similar to Integer Data type
+# a = 2 #int
+# b = 2.0 #float
+# c = 2.351 #float
+
+#Type Function
+a = 2
+b = 2.0
+c = '2.0'
+d = True
+e = False
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+a = 42
+b = float(a)
+c = str(a)
+print(a,b,c)
+
+#In which Data Type Input function return the value
