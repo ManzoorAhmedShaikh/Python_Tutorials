3664f7c4d27767c9d5b319db19203e954b952a7f
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-04 08:48:09 +0500

    Single and Multi-line Comments Updated

---
+++ Single_And_Multiline_Comments/Lecture_5.py
@@ -0,0 +1,33 @@
+#Block of Code
+a = input("Enter some value:")
+print("a =",a)
+
+#Single Line Comment
+a = input("Enter some value:")
+print("a =",a)
+
+#Multi-line Comment
+
+"""
+adasd
+as
+dad
+ads
+"""
+
+'''
+asdas
+asd
+adas
+das
+da
+da
+das
+das
+das
+das
+da
+'''
+
+# It is related to some value related to xyz work
+b = 23
