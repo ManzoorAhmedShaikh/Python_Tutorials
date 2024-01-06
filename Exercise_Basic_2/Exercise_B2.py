cec405db8b20461d454d02d89ca412438b447d01
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-06 08:47:42 +0500

    Exercise # 2 Updated

---
+++ Exercise_Basic_2/Exercise_B2.py
@@ -0,0 +1,25 @@
+# #Ask a user
+# a = input("Enter something")
+#
+# #Initialize
+# a = 2
+#
+
+#Problem
+
+a = "23.2"
+
+#Converting
+b = float(a)
+print(b)
+print(type(a))
+print(type(b))
+
+print()
+#Converting #2
+c = "23.2"
+
+print(type(c))
+c = float(c)
+print(c)
+print(type(c))
