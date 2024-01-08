b8eb1be196102c9dda63e828871482061bfd3e7f
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-08 10:14:57 +0500

    Arithmetic Operations Updated

---
+++ Arithmetic_Operators/Lecture_6.py
@@ -0,0 +1,50 @@
+#Operators
+
+#Example:
+
+'''Variable1 <OPERATOR> VARIABLE2'''
+'''Expression1 <OPERATOR> Expression2'''
+
+# eq = 2a + b
+# 2a + b OPERATOR 2a - b
+
+#Arithemtic Operators
+
+"""
+Addition        "+"
+Subtraction     "-"
+Multiplication  "*"
+Division        "/"
+Modulus         "%"
+Power           "**"
+"""
+
+a = 2
+b = 3
+c = 4
+
+print(a + b + c)
+print(a + b - c)
+print(a * b)
+print(c / a)
+print(c % a)
+print(a % c)
+
+print(5**2)
+print(5**3)
+
+#Exercise
+
+#1)
+b = 2
+
+eq = 1 - b/2
+print(eq)
+
+#2)
+b = 3
+c = 4
+d = 5
+
+eq2 = 24*b - c*d
+print(eq2)
