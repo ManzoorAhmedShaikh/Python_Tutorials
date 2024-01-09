41b52cc83d7b53a9959c0495c900882889b9bbec
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-09 09:20:40 +0500

    Relational Operators Updated

---
+++ Relational_Operators/Lecture_7.py
@@ -0,0 +1,35 @@
+#Relational Operators
+
+#Output
+#TRUE or FALSE
+
+# Greater Than              ">"
+# Less Than                 "<"
+# Greater Than Equal To     ">="
+# Less Than Equal To        ">="
+# Equal To                  "=="
+# Not Equal To              "!="
+
+#Syntax
+
+"""
+Statement1 <Relational Operator> Statement2
+"""
+
+a = 3
+b = 3
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+"""
+Q1) Write a program in which 
+you have to add 2 random numbers and check 
+if they are equal to 12 or not?
+"""
+
+print(4 + 8 == 12)
