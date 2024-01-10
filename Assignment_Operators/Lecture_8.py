f82dd26e8f3ee600e85d066ef4ad3703a755e2fc
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-10 08:47:55 +0500

    Assignment Operators Updated

---
+++ Assignment_Operators/Lecture_8.py
@@ -0,0 +1,25 @@
+#Assignment Operators
+
+#Basic Assignment Operator
+'='
+
+a = input("Enter some value")
+b = 2
+C = '2'
+
+#Concatenated Assignment Operator
+"""
+a = a + 2       |   a += 2
+a = a - 2       |   a -= 2
+a = a * 2       |   a *= 2
+a = a / 2       |   a /= 2
+a = a % 2       |   a %= 2
+"""
+
+a = 2
+a = a + 5
+print(a)
+
+a = 2
+a += 5
+print(a)
