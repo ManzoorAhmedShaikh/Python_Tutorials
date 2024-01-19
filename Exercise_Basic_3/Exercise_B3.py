0cc14c33fee8ef49bcd37c912c3ee650d8fd6a0d
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-19 08:52:09 +0500

    Exercise 3 Updated

---
+++ Exercise_Basic_3/Exercise_B3.py
@@ -0,0 +1,20 @@
+#Exercise 3
+
+#Q1
+# x = 2
+# y = 3
+#
+# eq1 = x + y**x
+# print(eq1)
+
+
+# x = 2
+# y = 3
+#
+# eq2 = 2 - (35*y)/x - 11*x
+# print(eq2)
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+#
+# print(a != b)
