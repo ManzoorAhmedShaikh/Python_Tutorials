0ed6c3b7004ea18862616fc6f75c7bbbb42d55dc
ManzoorAhmedShaikh <manzoorahmedshaikh234@gmail.com>
2024-01-20 14:38:42 +0500

    Keywords Updated

---
+++ Keywords_And_Usage/Lecture_11.py
@@ -0,0 +1,34 @@
+#Keywords
+
+#I/O
+# input()
+# print()
+
+# str()
+
+a = 2
+b = -2
+c = 'Manzoor Ahmed'
+
+print(a)
+print(abs(b))
+
+print(dir())
+
+print(id(a))
+print(id(b))
+print(len(c))
+
+print(max(2,3,5,6,22,4,66,12))
+print(min(2,3,5,6,22,4,66,12))
+
+print(a**4)
+print(pow(a,4))
+
+a = 3
+print(isinstance(a,str))
+
+a = 2.64123
+print(round(a,2))
+
+print("This is a Variable: {}".format(a))
